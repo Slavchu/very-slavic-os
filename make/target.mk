fe748662454c13a30c58b282e071fc86361c1664
Slavchu <tothelove1@gmail.com>
2026-03-15 19:41:40 +0200

    [esp32][flash]: replacing deprecated params

--- make/target.mk
+++ make/target.mk
@@ -8,7 +8,7 @@ endif
 
 #Flashing
 ifeq ($(TARGET), esp32_c3)
-    FLASH_CMD = esptool.py --chip esp32c3 --port /dev/ttyACM0 write_flash -fm dio 0x0 build/core.bin
+    FLASH_CMD = esptool --chip esp32c3 --port /dev/ttyACM0 write-flash -fm dio 0x0 build/core.bin
 else
     FLASH_CMD = @echo "No flash command defined for TARGET=$(TARGET). Skipping."
 endif
