7f1e16b0648212f2696e6f04489e8961afa6d703
Slavchu <tothelove1@gmail.com>
2026-03-17 12:37:58 +0200

    [riscv][bootloader]: disable watchdogs; print boot message

--- hal/RISC_V/riscv_entry.s
+++ hal/RISC_V/riscv_entry.s
@@ -2,6 +2,19 @@
 # is just because I do not understand what shit
 # is happening here. It is my first experience in
 # assembly.
+
+# global variables provided by linker
+.global UART0
+.global RTCCNTL
+.global TIMERG0
+.global TIMERG1
+
+# constants
+   .section .rodata
+boot_msg:
+    .asciz "Very slavic os bootloader\r\nBooted successfuly\r\n"
+
+# section code
   .section .init
   .global _start
   .type _start, @function
@@ -26,4 +39,56 @@ clear_bss_loop:
   blt t0, t1, clear_bss_loop
 
 end_clear_bss:
-  call main
+disable_rtc_watchdog:
+    la t0, RTCCNTL
+
+    li t1, 0x50D83AA1
+    sw t1, 0xA8(t0)       
+    sw zero, 0x90(t0)
+    sw zero, 0x8C(t0)
+
+    # --- FEED superwatchdog
+    li t1, 0x8F1D312A
+    sw t1, 0xB0(t0)
+
+    lw t1, 0xAC(t0)
+    li t2, 0x80000000
+    or t1, t1, t2
+    sw t1, 0xAC(t0)
+
+    sw zero, 0xB4(t0)
+
+    # ---TIMER GROUP 0 WDT
+    la t0, TIMERG0
+
+    lw t1, 0xFC(t0)
+    li t2, 0xFFFFFDFF
+    and t1, t1, t2
+    sw t1, 0xFC(t0)
+    sw zero, 0x48(t0)
+
+
+    # --- TIMER GROUP 1 WDT
+    la t0, TIMERG1
+    sw zero, 0x48(t0)
+
+print_boot_message:
+  la t0, UART0
+  la t1, boot_msg
+print_loop:
+  lbu t2, 0(t1)
+  beqz t2, print_done
+wait_fifo:
+  lw t3, 0x1C(t0)
+  srli t3, t3, 16
+  andi t3, t3, 0xFF
+  li t4, 120
+  bge t3, t4, wait_fifo
+
+  sw t2, 0(t0)
+  addi t1, t1, 1
+  j print_loop
+print_done:
+
+execute_core:
+call main
